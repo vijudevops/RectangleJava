50f71dbb253621c05848d1525d9ee042d36a844f
VijayKV <vijay.vadagole@candelalabs.io>
2021-08-10 15:27:44 +0530

    Fixed build xml docs dir, compile java issues

--- src/Rectangulator.java
+++ src/Rectangulator.java
@@ -1,10 +1,10 @@
 public class Rectangulator{
-    public static void mani(String[] args) {
+    public static void main(String[] args) {
         int lenth = Integer.parseInt(args[0]);
         int width = Integer.parseInt(args[1]);
 
-        Rectangle instanceRect = new Rectangle (length, width);
-        String output = String.format(" ****Your Rectangle ***\n \nLenght: %d\n Width: %d \n Area: %d \n Perimeter: %d \n\n", instanceRect.Lenght, instanceRect.width, instanceRect.getArea, instanceRect.getPerimeter);
+        Rectangle instanceRect = new Rectangle (lenth, width);
+        String output = String.format(" ****Your Rectangle ***\n \nLength: %d\n Width: %d \n Area: %d \n Perimeter: %d \n\n", instanceRect.length, instanceRect.width, instanceRect.getArea(), instanceRect.getPerimeter());
 
         System.out.println(output);
     }
